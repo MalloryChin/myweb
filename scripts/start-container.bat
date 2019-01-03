1b3584e1536ffa2cd9fe3b6757f8ee998a9389e5
Daniel Pittman <daniel@pittmo.net>
2019-01-02 19:04:41 -0700

    Removed unnecessary file creation from start scripts

--- scripts/start-container.bat
+++ scripts/start-container.bat
@@ -1,7 +1,3 @@
-type NUL > ..\data\root\.gitconfig
-type NUL > ..\data\root\.git-credentials
-type NUL > ..\data\root\.netrc
-
 docker-compose build
 docker-compose up
 
--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-touch ../data/root/.gitconfig
-touch ../data/root/.git-credentials
-touch ../data/root/.netrc
-
 docker-compose build
 docker-compose up
 
